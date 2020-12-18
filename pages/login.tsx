17e812339a2caa8f5380748b1a8ccf2144ce5b80
knowdeep <worstgrim@gmail.com>
2020-12-18 20:50:26 +0700

    styles js vs css

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,23 +1,20 @@
 import { createStyles, makeStyles } from "@material-ui/core";
 import React, { ReactElement } from "react";
+import loginStyle from "../styles/login.style";
+import loginCSS from "../public/static/css/login.module.css";
 
 interface Props {}
 
-const styles = createStyles({
-  section: {
-    color: "red",
-    fontSize: 100,
-  },
-});
-
-const useStyles = makeStyles(styles);
+const useStyles = makeStyles(loginStyle);
 
 export default function login({}: Props): ReactElement {
   const classed = useStyles();
   return (
     <div>
       <h1>Login</h1>
-      <div className={classed.section}>test</div>
+      <div className={classed.title}>test</div>
+      <div className={classed.subtitle}>subtitle</div>
+      <div className={loginCSS.section}>Bro Bro</div>
     </div>
   );
 }
---
+++ styles/login.style.js
@@ -0,0 +1,15 @@
+import { createStyles } from "@material-ui/core";
+export default createStyles({
+  section: {
+    color: "green",
+    fontSize: 100,
+  },
+  title: {
+    fontWeight: 900,
+    fontSize: 30,
+  },
+  subtitle: {
+    color: "blue",
+    fontSize: 50,
+  },
+});
