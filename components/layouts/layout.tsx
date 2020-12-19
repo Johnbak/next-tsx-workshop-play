66684ded3dd27cae092c31efdd28a6e9a7c57991
knowdeep <worstgrim@gmail.com>
2020-12-19 15:53:17 +0700

    logout - appBar icon

--- components/layouts/layout.tsx
+++ components/layouts/layout.tsx
@@ -1,3 +1,10 @@
+import {
+  Container,
+  createStyles,
+  makeStyles,
+  Theme,
+  Toolbar,
+} from "@material-ui/core";
 import React, { ReactElement, ReactNode } from "react";
 import Footer from "./footer";
 import Header from "./header";
@@ -7,12 +14,43 @@ interface Props {
   children: ReactNode; //props
 }
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: "flex",
+    },
+    appBar: {
+      zIndex: theme.zIndex.drawer + 1,
+    },
+    drawerContainer: {
+      overflow: "auto",
+    },
+    content: {
+      flexGrow: 1,
+      padding: theme.spacing(5),
+      paddingLeft: 300,
+    },
+  })
+);
+
 export default function Layout({ children }: Props): ReactElement {
+  const classes = useStyles();
   return (
     <React.Fragment>
       <Header />
       <Menu />
-      {children}
+      <main className={classes.content}>
+        <Toolbar />
+        <Container
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            backgroundColor: "red",
+          }}
+        >
+          {children}
+        </Container>
+      </main>
       <Footer
         style={{
           position: "fixed",
