14f7db094653964531618abdca551ffdbb93f4b8
knowdeep <worstgrim@gmail.com>
2020-12-23 21:26:57 +0700

    Edit get value from server

--- pages/stock/edit.tsx
+++ pages/stock/edit.tsx
@@ -1,24 +1,182 @@
-import { GetServerSideProps } from "next";
 import React, { ReactElement } from "react";
+import Header from "../../components/layouts/header";
+import Menu from "../../components/layouts/menu";
 import Layout from "../../components/layouts/layout";
+import MaterialTable, { Action } from "material-table";
+import { products } from "../api/dummy";
+import { Typography, makeStyles } from "@material-ui/core";
+import Moment from "react-moment";
+import NumberFormat from "react-number-format";
+import { Edit, DeleteOutline } from "@material-ui/icons";
+import { Formik, Form, Field } from "formik";
+import Container from "@material-ui/core/Container";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import { TextField } from "formik-material-ui";
+import { GetServerSideProps } from "next";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "80%",
+  },
+  field: {
+    marginTop: 16,
+  },
+  card: {
+    padding: 20,
+  },
+}));
 
 interface Props {
-  data: number[];
+  id: string;
+  name: string;
+  price: number;
+  stock: number;
 }
 
-export default function edit({ data }: Props): ReactElement {
+export default function StockEdit({
+  id,
+  name,
+  price,
+  stock,
+}: Props): ReactElement {
+  const classes = useStyles();
+
+  const showPreviewImage = (values) => {
+    if (values.file_obj) {
+      return (
+        <img src={values.file_obj} style={{ height: 100, marginTop: 16 }} />
+      );
+    }
+  };
+
+  const showForm = ({ values, setFieldValue, isValid, dirty }) => {
+    return (
+      <Form>
+        <Card className={classes.card}>
+          <CardContent>
+            <Typography gutterBottom variant="h3">
+              Edit Stock
+            </Typography>
+            <Field
+              className={classes.field}
+              component={TextField}
+              name="id"
+              type="text"
+              label="ProductId"
+              disabled
+            />
+            <br />
+            <Field
+              className={classes.field}
+              fullWidth
+              component={TextField}
+              name="name"
+              type="text"
+              label="Name"
+            />
+            <br />
+            <Field
+              className={classes.field}
+              fullWidth
+              component={TextField}
+              name="price"
+              type="number"
+              label="Price"
+            />
+
+            <Field
+              className={classes.field}
+              fullWidth
+              component={TextField}
+              name="stock"
+              type="number"
+              label="Stock"
+            />
+
+            <div>{showPreviewImage(values)}</div>
+
+            <div className={classes.field}>
+              <img
+                src={`/static/img/icon_camera.jpg`}
+                style={{ width: 25, height: 20 }}
+              />
+              <span
+                style={{ color: "#00B0CD", marginLeft: 10, marginRight: 10 }}
+              >
+                Add Picture
+              </span>
+              <input
+                type="file"
+                onChange={(e) => {
+                  e.preventDefault(); //Protect When Browse Refresh Page
+                  setFieldValue("file", e.target.files[0]); // for upload
+                  setFieldValue(
+                    "file_obj",
+                    URL.createObjectURL(e.target.files[0])
+                  ); // for preview image
+                }}
+                name="image"
+                click-type="type1"
+                className="picupload"
+                multiple
+                accept="image/*"
+                id="files"
+                style={{ padding: "20px 0" }}
+              />
+            </div>
+          </CardContent>
+          <CardActions>
+            <Button
+              variant="contained"
+              color="primary"
+              type="submit"
+              disabled={!(isValid && dirty)}
+            >
+              Create
+            </Button>
+            <Button>Cancel</Button>
+          </CardActions>
+        </Card>
+      </Form>
+    );
+  };
+
   return (
     <Layout>
-      <div>
-        <h1>Edit</h1>
-        <span>{JSON.stringify(data)}</span>
-      </div>
+      <Formik
+        validate={(values) => {
+          let errors: any = {};
+          if (!values.name) errors.name = "Please Enter Name";
+          if (!values.price) errors.price = "Please Enter Price";
+          if (values.stock <= 0) errors.stock = "Please Enter Stock";
+          return errors;
+        }}
+        enableReinitialize //enable Intitialz
+        initialValues={{ id, name, price, stock }}
+        onSubmit={(values: any, { setSubmitting }) => {
+          alert(JSON.stringify(values));
+          setSubmitting(true);
+          setTimeout(() => {
+            setSubmitting(false);
+          }, 2000);
+        }}
+      >
+        {(props) => showForm(props)}
+      </Formik>
     </Layout>
   );
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   return {
-    props: { data: [1, 2, 3, 4, parseInt(context.query.id.toString())] },
+    props: {
+      id: context.query.id,
+      name: "",
+      price: 999,
+      stock: 1,
+    },
   };
 };
