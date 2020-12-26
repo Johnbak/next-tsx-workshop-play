33a868ad84776c583326cc6b3de630f27260d570
knowdeep <worstgrim@gmail.com>
2020-12-26 16:40:06 +0700

    nodejs crud

--- server/server.js
+++ server/server.js
@@ -1,9 +1,24 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-app.get("/", (req, res) => {
-  res.json({ result: "esesssssssssssss" });
-});
+const router = express.Router();
+const product = require("./models/product");
+const Sequelize = require("sequelize");
+const constants = require("./constant");
+const formidable = require("formidable");
+const path = require("path");
+const fs = require("fs-extra");
+const Op = Sequelize.Op;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(__dirname + "/uploaded"));
+app.use(cors());
+
+app.use("/api/v2/authen/", require("./api_authen"));
+app.use("/api/v2/stock/", require("./api_stock"));
 
 app.listen(8085, () => {
   console.log("Baxk end is ready bro!");
